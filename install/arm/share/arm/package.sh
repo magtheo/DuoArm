bfa5e2de2d6bd71b5a35c8eb11511ba5688ec224
magtheo <theo@magnor.as>
2024-04-10 10:44:13 +0200

    removed install/build in howes of it fixing some error.

--- install/arm/share/arm/package.sh
+++ install/arm/share/arm/package.sh
@@ -78,7 +78,6 @@ _colcon_package_sh_source_script() {
 }
 
 # source sh hooks
-_colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/arm/hook/cmake_prefix_path.sh"
 _colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/arm/hook/pythonpath.sh"
 _colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/arm/hook/ament_prefix_path.sh"
 
--- install/path_planning/share/path_planning/package.sh
+++ install/path_planning/share/path_planning/package.sh
@@ -78,7 +78,6 @@ _colcon_package_sh_source_script() {
 }
 
 # source sh hooks
-_colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/path_planning/hook/cmake_prefix_path.sh"
 _colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/path_planning/hook/pythonpath.sh"
 _colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/path_planning/hook/ament_prefix_path.sh"
 
