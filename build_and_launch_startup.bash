781abd4ee3e7e58eaaa5b0e544a200a6f1ff9b55
Theo <theo@magnor.as>
2024-05-07 12:30:11 +0200

    new launch script

---
+++ build_and_launch_startup.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd Documents/DuoArm/
+
+source /opt/ros/humble/setup.bash
+source install/setup.bash
+
+colcon build
+
+source /opt/ros/humble/setup.bash
+source install/setup.bash
+
+ros2 launch control_center start_launch.py
